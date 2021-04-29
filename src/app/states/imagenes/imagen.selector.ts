721590929f4d8aab668cd19663bd7148d567b80b
EstevenJaviier <estirastam.c@gmail.com>
2021-04-29 10:51:11 -0500

    20210429

--- src/app/states/imagenes/imagen.selector.ts
+++ src/app/states/imagenes/imagen.selector.ts
@@ -4,13 +4,13 @@ import { ImagenState } from './imagen.state';
 const getImagenState = createFeatureSelector<ImagenState>('imagenes');
 
 export const selectGetImagenes = createSelector(
-  (state: { hitState: ImagenState }) => state.hitState,
+  (state: { imagenState: ImagenState }) => state.imagenState,
   getImagenState
 );
 
 export const selectGetImagenById = createSelector(
-  (state: { hitState: ImagenState }, props: any) => {
-    return state.hitState.imagenes.find((imagen) => imagen.id === props.id);
+  (state: { imagenState: ImagenState }, props: any) => {
+    return state.imagenState.imagenes.find((imagen) => imagen.id === props.id);
   },
   getImagenState
 );
